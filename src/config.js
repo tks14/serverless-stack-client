44a5b3e75c76d6fbf8ca417693b0521de6ccd766
vm <tks14@yahoo.com>
2020-09-15 16:36:16 -0400

    Configuring AWS amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "simple-post-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "YOUR_API_GATEWAY_URLhttps://ejvgnjhxfc.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_XoZObaBjQ",
+      APP_CLIENT_ID: "7m3panhclvv41vbi6h4cnqa0q2",
+      IDENTITY_POOL_ID: "us-east-2:672ab073-4454-4506-816d-9276ab0b48d4"
+    }
+  };
