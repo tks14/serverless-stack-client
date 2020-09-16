ecfaea1e7455cb9517e716b3fb026a9ac20c5115
vm <tks14@yahoo.com>
2020-09-16 05:13:31 -0400

    login and logout from server added

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
+import Login from "./containers/Login";
 
 export default function Routes() {
   return (
@@ -9,8 +10,9 @@ export default function Routes() {
         <Route exact path="/">
             <Home />
         </Route>
-      {/* Finally, catch all unmatched routes */}
-        {/* Finally, catch all unmatched routes */}
+        <Route exact path="/login">
+            <Login />
+        </Route>     
         <Route>
             <NotFound />
         </Route>
